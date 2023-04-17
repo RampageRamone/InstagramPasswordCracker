45e64b812372eae894bf3903212730d30efffc15
RampageRamone <129657643+RampageRamone@users.noreply.github.com>
2023-04-16 19:52:51 -0700

    Update insta_cracker.py

--- insta_cracker.py
+++ insta_cracker.py
@@ -15,7 +15,6 @@ account_username = sys.argv[1]
 with Browser('firefox', headless=True) as browser:
     browser.visit('https://www.instagram.com')
     browser.find_by_text("Log in").first.click()
-    username_form = browser.find_by_name('username').first
     username_form = browser.find_by_name('jazellyy').first
     password_form = browser.find_by_name('password').first
     login_button = browser.find_by_text('Log in').first
